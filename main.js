7b55f564918fb361cff95e8aa3bcc92ec341a706
Lindsay <lauder@usc.edu>
2017-12-08 01:44:48 -0800

    update w js

--- main.js
+++ main.js
@@ -1,43 +1,92 @@
-$(function () {
+$(document).ready(function(){
+  // Add smooth scrolling to all links
+  $("a").on('click', function(event) {
 
-	$("#oneText").textfill({
-	
-		explicitHeight: 25vh,
+    // Make sure this.hash has a value before overriding default behavior
+    if (this.hash !== "") {
+      // Prevent default anchor click behavior
+      event.preventDefault();
 
-	});
-	
+      // Store hash
+      var hash = this.hash;
+
+      // Using jQuery's animate() method to add smooth page scroll
+      // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+      $('html, body').animate({
+        scrollTop: $(hash).offset().top
+      }, 800, function(){
+   
+        // Add hash (#) to URL when done scrolling (default click behavior)
+        window.location.hash = hash;
+      });
+    } // End if
+  });
+});
+
+//smooth scroll w3schools tutorial: https://www.w3schools.com/jquery/tryit.asp?filename=tryjquery_eff_animate_smoothscroll
+
+
+$('#one').mouseover(function(){
+	$("#one").css('text-shadow', '2px 2px #C7C5B2');
+});
+
+$('#one').mouseout(function(){
+	$("#one").css('text-shadow', 'none');
+});
+
+$('#two').mouseover(function(){
+	$("#two").css('text-shadow', '2px 2px #807F3C');
+});
+
+$('#two').mouseout(function(){
+	$("#two").css('text-shadow', 'none');
+});
+
+$('#about').mouseover(function(){
+	$("#about").css('text-shadow', '2px 2px gray');
+});
+
+$('#about').mouseout(function(){
+	$("#about").css('text-shadow', 'none');
+});
+
+$('#title1').mouseover(function(){
+	$("#title1").css('text-shadow', '2px 2px #C7C5B2');
+});
+
+$('#title1').mouseout(function(){
+	$("#title1").css('text-shadow', 'none');
+});
+
+$('#oneText').mouseover(function(){
+	$("#oneText").css('box-shadow', '4px 4px #807F3C');
+});
+
+$('#oneText').mouseout(function(){
+	$("#oneText").css('box-shadow', 'none');
+});
+
+$('#twoText').mouseover(function(){
+	$("#twoText").css('box-shadow', '4px 4px #807F3C');
+});
+
+$('#twoText').mouseout(function(){
+	$("#twoText").css('box-shadow', 'none');
 });
 
 
-//var autoSizeText;
-//
-//autoSizeText = function() {
-//  var el, elements, _i, _len, _results;
-//  elements = $('.resize');
-//  console.log(elements);
-//  if (elements.length < 0) {
-//    return;
-//  }
-//  _results = [];
-//  for (_i = 0, _len = elements.length; _i < _len; _i++) {
-//    el = elements[_i];
-//    _results.push((function(el) {
-//      var resizeText, _results1;
-//      resizeText = function() {
-//        var elNewFontSize;
-//        elNewFontSize = (parseInt($(el).css('font-size').slice(0, -2)) - 1) + 'px';
-//        return $(el).css('font-size', elNewFontSize);
-//      };
-//      _results1 = [];
-//      while (el.scrollHeight > el.offsetHeight) {
-//        _results1.push(resizeText());
-//      }
-//       return _results1;
-//    })(el));
-//  }
-//  return _results;
-//};
-//
-//$(document).ready(function() {
-//  return autoSizeText();
-//});
+$('#issue2Img').mouseover(function(){
+	$("#issue2Img").css('box-shadow', '-4px -4px #C7C5B2');
+});
+
+$('#issue2Img').mouseout(function(){
+	$("#issue2Img").css('box-shadow', 'none');
+});
+
+$('#issue1Img').mouseover(function(){
+	$("#issue1Img").css('box-shadow', '-4px -4px #C7C5B2');
+});
+
+$('#issue1Img').mouseout(function(){
+	$("#issue1Img").css('box-shadow', 'none');
+});
